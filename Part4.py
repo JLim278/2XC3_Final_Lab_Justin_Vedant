31b5c931c38fa0fc9544b7f9d013c2712f843bee
Vedant Patel <vedantpatel375@gmail.com>
2026-04-04 23:22:04 -0400

    part 4

---
+++ Part4.py
@@ -0,0 +1,72 @@
+from final_project_part1 import DirectedWeightedGraph, dijkstra, bellman_ford
+from part2_implementation import a_star
+
+
+class Graph:
+    def adjacent_nodes(self, node):
+        pass
+
+    def add_node(self, node):
+        pass
+
+    def add_edge(self, node1, node2, weight):
+        pass
+
+    def number_of_nodes(self):
+        pass
+
+
+class WeightedGraph(DirectedWeightedGraph, Graph):
+    pass
+
+
+class HeuristicGraph(WeightedGraph):
+    def __init__(self):
+        super().__init__()
+        self.heuristic = {}
+
+    def set_heuristic(self, heuristic):
+        self.heuristic = heuristic
+
+    def get_heuristic(self):
+        return self.heuristic
+
+
+class SPAlgorithm:
+    def calc_sp(self, graph, source, dest):
+        pass
+
+
+class Dijkstra(SPAlgorithm):
+    def calc_sp(self, graph, source, dest):
+        return dijkstra(graph, source)
+
+
+class Bellman_Ford(SPAlgorithm):
+    def calc_sp(self, graph, source, dest):
+        return bellman_ford(graph, source)
+
+
+class A_Star(SPAlgorithm):
+    def calc_sp(self, graph, source, dest):
+        converted_graph = {}
+        for node in graph.adj:
+            converted_graph[node] = {}
+            for neighbour in graph.adj[node]:
+                converted_graph[node][neighbour] = graph.w(node, neighbour)
+        return a_star(converted_graph, source, dest, graph.get_heuristic())
+
+
+class ShortPathFinder:
+    def __init__(self):
+        self.graph = None
+        self.algorithm = None
+
+    def set_graph(self, graph):
+        self.graph = graph
+
+    def set_algorithm(self, algorithm):
+        self.algorithm = algorithm
+
+    def calc_short_path(self, source, dest):
+        return self.algorithm.calc_sp(self.graph, source, dest)
